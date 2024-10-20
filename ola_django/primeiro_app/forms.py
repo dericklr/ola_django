d99deed61525e9567659f5ebcaf0de2e4234b725
Derick Rodrigues <dericklegunde@gmail.com>
2024-10-20 15:20:52 -0300

    commit
    conteudos de aula concluidos

---
+++ ola_django/primeiro_app/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Pessoa
+
+class PessoaCreateForm(forms.ModelForm):
+    interacao=forms.CharField(widget=forms.Textarea)
+    class Meta:
+        model= Pessoa
+        fields='__all__'
+
+class PessoaUpdateForm(forms.ModelForm):
+    interacao=forms.CharField(widget=forms.Textarea)
+    class Meta:
+        model=Pessoa
+        fields='__all__'
+
+class FormDeletePessoa(forms.ModelForm):
+    class Meta:
+        model=Pessoa
+        fields=[]                
+
+        
